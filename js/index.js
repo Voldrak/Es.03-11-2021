1ddadc24574ee1ed3253cad0b38c0636c926c09f
Marco Messina <voldrak94@gmail.com>
2021-11-03 17:15:46 +0100

    esercizio completo

--- js/index.js
+++ js/index.js
@@ -2,26 +2,32 @@
 const render = (container, items) => {
 
     const elements = items.map((element) =>
-        `<li> 
-        <input type="checkbox" name="completed" class="completed">
+    {
+    if (element.completed === true) {
+         return `<li> 
+        <input type="checkbox" name="completed" class="completed" value="completed" checked>
         <label>${element.title}</label>
         ${element.completed}
         </li>`
-    );
+    } else {
+        return `<li> 
+        <input type="checkbox" name="completed" class="completed" value="completed">
+        <label>${element.title}</label>
+        ${element.completed}
+        </li>`
+    }
+        
+});  
+    
     const content = elements.join('');
 
     container.innerHTML = content;
 
-    if (${element.completed} == true) {
-        
-    }
-
-
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const list = document.querySelector('ul');
 
     render(list, listT)
 });
-
